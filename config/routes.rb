78a49f6019b62728dfb2d304662b6be1b63badcc
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2019-12-27 16:08:32 +0900

    deviseインストール, pilotモデル作成, ルーティング, コントローラー作成, ログインしてるか確認できる様にインデックスに表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :pilots, controllers: {
+     sessions: "pilots/sessions",
+     passwords: "pilots/passwords",
+     registrations: "pilots/registrations"
+    }
   root to: "information#index"
 end
