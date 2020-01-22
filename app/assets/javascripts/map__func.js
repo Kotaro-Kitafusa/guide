39793474dafb7cfe9e47c6b5865cf760381fd18f
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2020-01-22 21:22:53 +0900

    mapページにアクセスした時のみmap__func.jsが走るよう記述。旅行者の位置を青い丸で表示

--- app/assets/javascripts/map__func.js
+++ app/assets/javascripts/map__func.js
@@ -1,65 +1,64 @@
 $(document).ready(function() {
+  if($('#map').length){
+      // function changeNickname(userId){
+      //   var userId = userId;
+      //   var nickname = "Kartman";
+      //   $.ajax({
+      //     url: "/map/change_pilot_nickname",
+      //     type: "PATCH",
+      //     data: {nickname: nickname},
+      //     dataType: 'json',
+      //   })
+      //   .done(function(nickname){
+      //     console.log(`successfully changed a username as ${nickname.nickname}`);
+      //   })
+      //   .fail(function(){
+      //     console.log('Failed!');
+      //   })
+      // }
+      function saveCurrentLocation(lat, lng){
+        // console.log(lat, lng)
+        var lat = lat;
+        var lng = lng;
+        $.ajax({
+          url: "/map/change_pilot_location",
+          type: "PATCH",
+          data: {lat: lat, lng:lng},
+          dataType: 'json',
+        })
+        .done(function(location){
+          console.log(`successfully changed userlodation as ${location.lat}, ${location.lng}`);
+        })
+        .fail(function(){
+          console.log('Failed!');
+        })
+      }
+      function successPos(pos){
+        var lat = pos.coords.latitude;
+        var lng = pos.coords.longitude;
+        var location = document.querySelector('.show-lati-and-long');
+        location.innerHTML = `<p class = "location">緯度: ${pos.coords.latitude}</p>
+        <p class = "location">経度: ${pos.coords.longitude}</p>`;
+        var mwi = document.querySelector('.map-wrapper__in');
+        mwi.appendChild(location);
+        saveCurrentLocation(lat, lng);
+      }
+      function errorPos(err){
+        console.log(err.message);
+      }
+      // check this out for options
+      // https://stackoverflow.com/questions/16202077/high-accuracy-geolocation-html5
+      // , timeout:5000
+      // maximumAge:10000,
+      enableHighAccuracy: true
+      var posOptions = {enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity }
 
-  // function changeNickname(userId){
-  //   var userId = userId;
-  //   var nickname = "Kartman";
-  //   $.ajax({
-  //     url: "/map/change_pilot_nickname",
-  //     type: "PATCH",
-  //     data: {nickname: nickname},
-  //     dataType: 'json',
-  //   })
-  //   .done(function(nickname){
-  //     console.log(`successfully changed a username as ${nickname.nickname}`);
-  //   })
-  //   .fail(function(){
-  //     console.log('Failed!');
-  //   })
-  // }
-
-  function saveCurrentLocation(lat, lng){
-    // console.log(lat, lng)
-    var lat = lat;
-    var lng = lng;
-    $.ajax({
-      url: "/map/change_pilot_location",
-      type: "PATCH",
-      data: {lat: lat, lng:lng},
-      dataType: 'json',
-    })
-    .done(function(location){
-      console.log(`successfully changed userlodation as ${location.lat}, ${location.lng}`);
-    })
-    .fail(function(){
-      console.log('Failed!');
-    })
-  }
-
-  function successPos(pos){
-    var lat = pos.coords.latitude;
-    var lng = pos.coords.longitude;
-    var location = document.querySelector('.show-lati-and-long');
-    location.innerHTML = `<p class = "location">緯度: ${pos.coords.latitude}</p>
-    <p class = "location">経度: ${pos.coords.longitude}</p>`;
-    var mwi = document.querySelector('.map-wrapper__in');
-    mwi.appendChild(location);
-    saveCurrentLocation(lat, lng);
-  }
-  function errorPos(err){
-    console.log(err.message);
-  }
-  // check this out for options
-  // https://stackoverflow.com/questions/16202077/high-accuracy-geolocation-html5
-  // , timeout:5000
-  // maximumAge:10000,
-  enableHighAccuracy: true
-  var posOptions = {enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity }
-
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(successPos, errorPos, posOptions);
-  }
-  else {
-    console.log('it dosent work');
-    alert('nope');
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(successPos, errorPos, posOptions);
+      }
+      else {
+        console.log('it dosent work');
+        alert('nope');
+      }
   }
 });
