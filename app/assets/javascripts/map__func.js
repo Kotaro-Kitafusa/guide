8c10926f14e969ff2284cdf238b4ee778cfa76b8
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2020-01-24 17:54:40 +0900

    読み込んだ緯度経度を中心にマップ表示

--- app/assets/javascripts/map__func.js
+++ app/assets/javascripts/map__func.js
@@ -1,63 +1,64 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+$(window).load(function() {
   if($('#map').length){
-      // function changeNickname(userId){
-      //   var userId = userId;
-      //   var nickname = "Kartman";
+    var myLatLng;
+    var map;
+    var marker;
+
+    function initMap(){
+      mapOptions = { zoom: 15};
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
+      marker = new google.maps.Marker({
+        map: map,
+        zIndex:1
+        });
+    }
+    initMap();
+
+      // function saveCurrentLocation(lat, lng){
+      //   var lat = lat;
+      //   var lng = lng;
       //   $.ajax({
-      //     url: "/map/change_pilot_nickname",
+      //     url: "/map/change_pilot_location",
       //     type: "PATCH",
-      //     data: {nickname: nickname},
+      //     data: {lat: lat, lng:lng},
       //     dataType: 'json',
       //   })
-      //   .done(function(nickname){
-      //     console.log(`successfully changed a username as ${nickname.nickname}`);
+      //   .done(function(location){
+      //     console.log(`successfully changed userlocation as ${location.lat}, ${location.lng}`);
       //   })
       //   .fail(function(){
       //     console.log('Failed!');
       //   })
       // }
-      function saveCurrentLocation(lat, lng){
-        // console.log(lat, lng)
-        var lat = lat;
-        var lng = lng;
-        $.ajax({
-          url: "/map/change_pilot_location",
-          type: "PATCH",
-          data: {lat: lat, lng:lng},
-          dataType: 'json',
-        })
-        .done(function(location){
-          console.log(`successfully changed userlodation as ${location.lat}, ${location.lng}`);
-        })
-        .fail(function(){
-          console.log('Failed!');
-        })
+
+      function centerMap(pos){
+        console.log('cm act');
+        myLatLng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+        debugger;
+        map.setCenter(myLatLng);
+        map.setZoom(16);
+        marker.setPosition(myLatLng);
       }
-      function successPos(pos){
-        var lat = pos.coords.latitude;
-        var lng = pos.coords.longitude;
-        // var location = document.querySelector('.show-lati-and-long');
-        // location.innerHTML = `<p class = "location">緯度: ${pos.coords.latitude}</p>
-        // <p class = "location">経度: ${pos.coords.longitude}</p>`;
-        // var mwi = document.querySelector('.map-wrapper__in');
-        // mwi.appendChild(location);
-        saveCurrentLocation(lat, lng);
-      }
+
+      // function successPos(pos){
+      //   var lat = pos.coords.latitude;
+      //   var lng = pos.coords.longitude;
+      //   navigator.geolocation.watchPosition(centerMap);
+      //   saveCurrentLocation(lat, lng);
+      // }
+
       function errorPos(err){
         console.log(err.message);
       }
       // check this out for options
       // https://stackoverflow.com/questions/16202077/high-accuracy-geolocation-html5
-      // , timeout:5000
-      // maximumAge:10000,
-      enableHighAccuracy: true
-      var posOptions = {enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity }
+      var posOptions = {enableHighAccuracy: false, timeout: 150000, maximumAge: 10000 }
 
       if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(successPos, errorPos, posOptions);
+        navigator.geolocation.watchPosition(centerMap, errorPos, posOptions);
       }
       else {
-        console.log('it dosent work');
         alert('nope');
       }
   }
