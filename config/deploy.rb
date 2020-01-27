fd76140efdcc4f7da0088ad6d757cf881175e2a6
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2020-01-27 14:01:22 +0900

    本番環境用のmasterkey設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,20 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# master.keyをアップロードする設定
+desc 'upload master.key'
+task :upload do
+  on roles(:app) do |host|
+    if test "[ ! -d #{shared_path}/config ]"
+      execute "mkdir -p #{shared_path}/config"
+    end
+    upload!('config/master.key', "#{shared_path}/config/master.key")
+  end
+end
+before :starting, 'deploy:upload'
+after :finishing, 'deploy:cleanup'
+end
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
