ac6b0d038b6a8cd71b923a27386fd524dc65157b
Kotaro Kitafusa <puppet0pierrot@gmail.com>
2020-01-29 16:10:49 +0900

    暫定的な地図表示

--- app/assets/javascripts/map__func.js
+++ app/assets/javascripts/map__func.js
@@ -1,19 +1,22 @@
 // $(document).ready(function() {
-$(window).load(function() {
-  if($('#map').length){
-    var myLatLng;
-    var map;
-    var marker;
+/////////////////111111111/////////////////
+// $(window).load(function() {
+//   if($('#map').length){
+//     var myLatLng;
+//     var map;
+//     var marker;
 
-    function initMap(){
-      mapOptions = { zoom: 15};
-      map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      marker = new google.maps.Marker({
-        map: map,
-        zIndex:1
-        });
-    }
-    initMap();
+//     function initMap(){
+//       mapOptions = { zoom: 15};
+//       map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//       marker = new google.maps.Marker({
+//         map: map,
+//         zIndex:1
+//         });
+//     }
+    // initMap();
+/////////////////111111111/////////////////
+
 
       // function saveCurrentLocation(lat, lng){
       //   var lat = lat;
@@ -31,14 +34,16 @@ $(window).load(function() {
       //     console.log('Failed!');
       //   })
       // }
+/////////////////111111111/////////////////
 
-      function centerMap(pos){
-        console.log(pos.coords.latitude,pos.coords.longitude);
-        myLatLng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
-        map.setCenter(myLatLng);
-        map.setZoom(16);
-        marker.setPosition(myLatLng);
-      }
+      // function centerMap(pos){
+      //   console.log(pos.coords.latitude,pos.coords.longitude);
+      //   myLatLng = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+      //   map.setCenter(myLatLng);
+      //   map.setZoom(16);
+      //   marker.setPosition(myLatLng);
+      // }
+/////////////////111111111/////////////////
 
       // function successPos(pos){
       //   var lat = pos.coords.latitude;
@@ -46,19 +51,20 @@ $(window).load(function() {
       //   navigator.geolocation.watchPosition(centerMap);
       //   saveCurrentLocation(lat, lng);
       // }
+/////////////////111111111/////////////////
+//       function errorPos(err){
+//         console.log(err.message);
+//       }
+//       // check this out for options
+//       // https://stackoverflow.com/questions/16202077/high-accuracy-geolocation-html5
+//       var posOptions = {enableHighAccuracy: false, timeout: 150000, maximumAge: 10000 }
 
-      function errorPos(err){
-        console.log(err.message);
-      }
-      // check this out for options
-      // https://stackoverflow.com/questions/16202077/high-accuracy-geolocation-html5
-      var posOptions = {enableHighAccuracy: false, timeout: 150000, maximumAge: 10000 }
-
-      if(navigator.geolocation){
-        navigator.geolocation.watchPosition(centerMap, errorPos, posOptions);
-      }
-      else {
-        alert('nope');
-      }
-  }
-});
+//       if(navigator.geolocation){
+//         navigator.geolocation.watchPosition(centerMap, errorPos, posOptions);
+//       }
+//       else {
+//         alert('nope');
+//       }
+//   }
+// });
+/////////////////111111111/////////////////
